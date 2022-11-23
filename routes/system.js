e4d710737a0816159dcf6d96abf669179ed52c94
saymon-felipe <linnubr@gmail.com>
2022-11-22 22:38:34 -0300

    tarefa #183

--- routes/system.js
+++ routes/system.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const util = require('util');
 
 // Versão do sistema
-const system_version = "v " + "1.2.12";
+const system_version = "v " + "1.2.13";
 // Nome da plataforma
 const system_name = "Cademint";
 // Criador da plataforma
