903cd0a0861fcb0b066c5d33e0c223c034024736
Saymon Felipe <linnubr@gmail.com>
2023-08-22 15:55:18 -0300

    Tentativa de resolução do pool

--- routes/system.js
+++ routes/system.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Versão do sistema
-const system_version = "v " + "1.2.23";
+const system_version = "v " + "1.2.24";
 // Nome da plataforma
 const system_name = "Cademint";
 // Criador da plataforma
--- utils/functions.js
+++ utils/functions.js
@@ -4,10 +4,16 @@ let functions = {
     executeSql: function (query, queryParams = []) {
         return new Promise((resolve, reject) => {
             mysql.getConnection((error, conn) => {
-                if (error) { reject(error) };
+                if (error) {
+                    reject(error);
+                    return;
+                };
                 conn.query(query, queryParams, (err, results) => {
                     conn.release();
-                    if (err) { reject(err) };
+                    if (err) {
+                        reject(err);
+                        return;
+                    };
                     resolve(results);
                 })
             })
